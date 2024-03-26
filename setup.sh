99e929df1d70220120db971562b56d7d468bdfe5
Jeena Huneidi <jhuneidi@wikimedia.org>
2024-03-26 01:43:34 +0000

    Update setup.sh for Debian 12 Bookworm
    * Fixes permissions issue
    * Updates nodejs to node 18
    * Updates to php 8.2
    
    See #611

--- setup.sh
+++ setup.sh
@@ -1,23 +1,24 @@
 # dependencies of MediaWiki
 sudo apt-get install apache2 default-mysql-server php libapache2-mod-php php-mysql php-intl php-xml php-mbstring php-curl php-gd php-wikidiff2 imagemagick librsvg2-bin lame
 # dependencies of our system
-sudo apt-get install git composer npm unzip rdfind curl
+sudo apt-get install composer npm unzip rdfind curl
 
-# Node 16
-sudo curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
-sudo sh -c "echo deb https://deb.nodesource.com/node_16.x impish main > /etc/apt/sources.list.d/nodesource.list"
+#Node 18
+sudo curl -fsSL https://deb.nodesource.com/setup_18.x | bash - &&\
 sudo apt-get update
-sudo apt-get install nodejs
+
+
 # Update NPM
-sudo npm install -g npm@latest
+sudo npm install npm@latest
 # Let www-data run NPM
-sudo mkdir /var/www/.npm /var/www/.config
+sudo mkdir -p /var/www/.npm /var/www/.config
 sudo chown www-data: /var/www/.npm /var/www/.config
 # We used to run NPM as root
 sudo chown -R www-data: node_modules
 
 # create master copies of repositories
-sudo -u www-data mkdir repositories
+sudo mkdir -p repositories
+sudo chown www-data: repositories
 cd repositories
 while IFS=' ' read -r repo dir; do
 	sudo -u www-data git clone --no-checkout https://gerrit.wikimedia.org/r/$repo.git $repo
@@ -25,10 +26,12 @@ done < ../repository-lists/all.txt
 cd ..
 
 # Composer wants a directory for itself (COMPOSER_HOME)
-sudo -u www-data mkdir composer
+sudo mkdir -p composer
+sudo chown www-data: composer
 
 # Create folder for wikis
-sudo -u www-data mkdir wikis
+sudo mkdir -p wikis
+sudo chown www-data: wikis
 
 # Create a database user that is allowed to create databases for each wiki,
 # and the central patchdemo database
@@ -57,7 +60,7 @@ session.cookie_lifetime = 1209600
 
 ; double the default memory limit
 memory_limit = 256M
-" > /etc/php/7.4/apache2/conf.d/patchdemo.ini
+" > /etc/php/8.2/apache2/conf.d/patchdemo.ini
 
 # enable .htaccess files
 echo "<Directory /var/www/html>
