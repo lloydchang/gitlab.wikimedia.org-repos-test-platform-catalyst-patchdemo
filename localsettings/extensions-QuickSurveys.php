6328052a14e1bc863932b33135bc6e7023e44b43
Jon Robson <jdlrobson@gmail.com>
2024-06-07 11:37:46 -0700

    Update QuickSurvey config
    Fixes: #605

--- localsettings/extensions-QuickSurveys.php
+++ localsettings/extensions-QuickSurveys.php
@@ -4,12 +4,27 @@ $wgQuickSurveysConfig = [
 	[
 		'name' => 'internal example survey',
 		'type' => 'internal',
-		'layout' => 'single-answer',
-		'question' => 'ext-quicksurveys-example-internal-survey-question',
-		'answers' => [
-			'ext-quicksurveys-example-internal-survey-answer-positive',
-			'ext-quicksurveys-example-internal-survey-answer-neutral',
-			'ext-quicksurveys-example-internal-survey-answer-negative',
+		'questions' => [
+			[
+				'name' => 'q1',
+				'question' => 'ext-quicksurveys-example-internal-survey-question',
+				// The respondent can choose one answer from a list.
+				'layout' => 'single-answer',
+				// The message key of the description of the survey. Displayed immediately below the survey question.
+				//'description' => 'ext-quicksurveys-example-internal-survey-description',
+				// Possible answer message keys for positive, neutral, and negative
+				'answers' => [
+					[
+						'label' => 'ext-quicksurveys-example-internal-survey-answer-positive',
+					],
+					[
+						'label' => 'ext-quicksurveys-example-internal-survey-answer-neutral',
+					],
+					[
+						'label' => 'ext-quicksurveys-example-internal-survey-answer-negative',
+					],
+				]
+			]
 		],
 		'enabled' => true,
 		'coverage' => 0,
@@ -21,15 +36,32 @@ $wgQuickSurveysConfig = [
 	[
 		'name' => 'internal example survey with description and freeform text',
 		'type' => 'internal',
-		'layout' => 'single-answer',
-		'question' => 'ext-quicksurveys-example-internal-survey-question',
-		'answers' => [
-			'ext-quicksurveys-example-internal-survey-answer-positive',
-			'ext-quicksurveys-example-internal-survey-answer-neutral',
-			'ext-quicksurveys-example-internal-survey-answer-negative',
+		'questions' => [
+			[
+				'name' => 'q1',
+				'question' => 'ext-quicksurveys-example-internal-survey-question',
+				// The respondent can choose one answer from a list.
+				'layout' => 'single-answer',
+				'description' => 'ext-quicksurveys-example-internal-survey-description',
+				// The message key of the description of the survey. Displayed immediately below the survey question.
+				//'description' => 'ext-quicksurveys-example-internal-survey-description',
+				// Possible answer message keys for positive, neutral, and negative
+				'answers' => [
+					[
+						'label' => 'ext-quicksurveys-example-internal-survey-answer-positive',
+						'freeformTextLabel' => 'ext-quicksurveys-example-internal-survey-freeform-text-label',
+					],
+					[
+						'label' => 'ext-quicksurveys-example-internal-survey-answer-neutral',
+						'freeformTextLabel' => 'ext-quicksurveys-example-internal-survey-freeform-text-label',
+					],
+					[
+						'label' => 'ext-quicksurveys-example-internal-survey-answer-negative',
+						'freeformTextLabel' => 'ext-quicksurveys-example-internal-survey-freeform-text-label',
+					],
+				]
+			]
 		],
-		'description' => 'ext-quicksurveys-example-internal-survey-description',
-		'freeformTextLabel' => 'ext-quicksurveys-example-internal-survey-freeform-text-label',
 		'enabled' => true,
 		'coverage' => 0,
 		'platforms' => [
@@ -40,47 +72,87 @@ $wgQuickSurveysConfig = [
 	[
 		'name' => 'internal multiple answer example survey',
 		'type' => 'internal',
-		'layout' => 'multiple-answer',
-		'question' => 'ext-quicksurveys-example-internal-survey-question',
-		'answers' => [
-			'ext-quicksurveys-example-internal-survey-answer-positive',
-			'ext-quicksurveys-example-internal-survey-answer-neutral',
-			'ext-quicksurveys-example-internal-survey-answer-negative',
+		'questions' => [
+			[
+				'name' => 'q1',
+				'question' => 'ext-quicksurveys-example-internal-survey-question',
+				// The respondent can choose one answer from a list.
+				'layout' => 'multiple-answer',
+				'description' => 'ext-quicksurveys-example-internal-survey-description',
+				// The message key of the description of the survey. Displayed immediately below the survey question.
+				//'description' => 'ext-quicksurveys-example-internal-survey-description',
+				// Possible answer message keys for positive, neutral, and negative
+				'answers' => [
+					[
+						'label' => 'ext-quicksurveys-example-internal-survey-answer-positive',
+						'freeformTextLabel' => 'ext-quicksurveys-example-internal-survey-freeform-text-label',
+					],
+					[
+						'label' => 'ext-quicksurveys-example-internal-survey-answer-neutral',
+						'freeformTextLabel' => 'ext-quicksurveys-example-internal-survey-freeform-text-label',
+					],
+					[
+						'label' => 'ext-quicksurveys-example-internal-survey-answer-negative',
+						'freeformTextLabel' => 'ext-quicksurveys-example-internal-survey-freeform-text-label',
+					],
+				]
+			]
 		],
+
 		'enabled' => true,
 		'coverage' => 0,
 		'platforms' => [
 			'desktop' => [ 'stable' ],
 			'mobile' => [ 'stable', 'beta' ],
 		],
-		'shuffleAnswersDisplay' => true,
 	],
 	[
 		'name' => 'internal multiple answer example survey with description and freeform text',
 		'type' => 'internal',
-		'layout' => 'multiple-answer',
-		'question' => 'ext-quicksurveys-example-internal-survey-question',
-		'answers' => [
-			'ext-quicksurveys-example-internal-survey-answer-positive',
-			'ext-quicksurveys-example-internal-survey-answer-neutral',
-			'ext-quicksurveys-example-internal-survey-answer-negative',
+		'questions' => [
+			[
+				'name' => 'q1',
+				'question' => 'ext-quicksurveys-example-internal-survey-question',
+				// The respondent can choose one answer from a list.
+				'layout' => 'multiple-answer',
+				'description' => 'ext-quicksurveys-example-internal-survey-description',
+				'shuffleAnswersDisplay' => true,
+				// The message key of the description of the survey. Displayed immediately below the survey question.
+				//'description' => 'ext-quicksurveys-example-internal-survey-description',
+				// Possible answer message keys for positive, neutral, and negative
+				'answers' => [
+					[
+						'label' => 'ext-quicksurveys-example-internal-survey-answer-positive',
+						'freeformTextLabel' => 'ext-quicksurveys-example-internal-survey-freeform-text-label',
+					],
+					[
+						'label' => 'ext-quicksurveys-example-internal-survey-answer-neutral',
+						'freeformTextLabel' => 'ext-quicksurveys-example-internal-survey-freeform-text-label',
+					],
+					[
+						'label' => 'ext-quicksurveys-example-internal-survey-answer-negative',
+						'freeformTextLabel' => 'ext-quicksurveys-example-internal-survey-freeform-text-label',
+					],
+				]
+			]
 		],
-		'description' => 'ext-quicksurveys-example-internal-survey-description',
-		'freeformTextLabel' => 'ext-quicksurveys-example-internal-survey-freeform-text-label',
 		'enabled' => true,
 		'coverage' => 0,
 		'platforms' => [
 			'desktop' => [ 'stable' ],
 			'mobile' => [ 'stable', 'beta' ],
 		],
-		'shuffleAnswersDisplay' => true,
 	],
 	[
 		'name' => 'external example survey',
 		'type' => 'external',
-		'question' => 'ext-quicksurveys-example-external-survey-question',
-		'description' => 'ext-quicksurveys-example-external-survey-description',
-		'link' => 'ext-quicksurveys-example-external-survey-link',
+		'questions' => [
+			[
+				'question' => 'ext-quicksurveys-example-external-survey-question',
+				'description' => 'ext-quicksurveys-example-external-survey-description',
+				'link' => 'ext-quicksurveys-example-external-survey-link',
+			]
+		],
 		'privacyPolicy' => 'ext-quicksurveys-example-external-survey-privacy-policy',
 		'coverage' => 0,
 		'enabled' => true,
