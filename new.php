lloydchang/gitlab.wikimedia.org-repos-test-platform-catalyst-patchdemo

commit 9482831549fa53503cde59cb7c15bab8173717cd

Fix streaming responses
I don't know why this is needed now and wasn't needed before,
but without it, the response on new.php is only sent when the
script is finished, not continuously.

new.php
@@ -6,10 +6,7 @@ require_once "includes.php";
 
 include "header.php";
 
-// Instruct the server to stream output immediately, without waiting for the script to finish
-ob_implicit_flush( true );
-// The streamed responses trigger some gzip encoding bug somewhere, so disable it (#609)
-apache_setenv( 'no-gzip', '1' );
+stream_response();
 
 $auth = Authentication::getInstance();
 if ( $auth->canSignIn() ) {

delete.php
@@ -73,10 +73,7 @@ if ( !isset( $_POST['csrf_token'] ) || !$auth->checkCsrfToken( $_POST['csrf_toke
 	error( "Invalid session." );
 }
 
-// Instruct the server to stream output immediately, without waiting for the script to finish
-ob_implicit_flush( true );
-// The streamed responses trigger some gzip encoding bug somewhere, so disable it (#609)
-apache_setenv( 'no-gzip', '1' );
+stream_response();
 
 echo '<div class="consoleLog">';
 $error = delete_wiki( $wiki );

includes.php
@@ -40,6 +40,16 @@ if ( $mysqli->connect_error ) {
 	error( $mysqli->connect_error );
 }
 
+function stream_response() {
+	// Instruct the server to stream output immediately, without waiting for the script to finish
+	ob_implicit_flush( true );
+	for ( $i = 0; $i < ob_get_level(); $i++ ) {
+		ob_end_flush();
+	}
+	// The streamed responses trigger some gzip encoding bug somewhere, so disable it (#609)
+	apache_setenv( 'no-gzip', '1' );
+}
+
 function insert_wiki_data( string $wiki, string $creator, int $created, string $branch, ?string $landingPage ) {
 	global $mysqli;
 	$stmt = $mysqli->prepare( '

commit 34ebd0e18cc6f7304ab8c9b52a456eabe6160eac

Fix "Notice: ob_flush(): failed to flush buffer. No buffer to flush"
Probably caused by 9482831549fa53503cde59cb7c15bab8173717cd.

new.php
@@ -79,7 +79,6 @@ function set_progress( float $pc, string $label ) {
 	echo '<p>' . htmlspecialchars( $label ) . '</p>';
 	$labelJson = json_encode_clean( $label );
 	echo "<script>pd.setProgress( $pc, $labelJson );</script>";
-	ob_flush();
 }
 
 echo new OOUI\FieldsetLayout( [