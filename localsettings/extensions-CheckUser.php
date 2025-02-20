a98536e8d498fbfbc5e8e47e6cc7e63975b73f89
Dreamy Jazz <dreamyjazzwikipedia@gmail.com>
2025-02-20 21:59:25 +0000

    CheckUser: Hide temp accounts onboarding dialog by default
    Why:
    * The CheckUser extension implements a onboarding dialog for temporary accounts that is shown to users with rights to reveal their IPs
    * This can be annoying for test wikis, as the user does not need to be informed how to use the tools
    * Hiding the dialog by default on patch demo wikis avoids the issue, while still allowing testing the dialog at a later time
    
    What:
    * Set `$wgDefaultUserOptions['checkuser-temporary-accounts-onboarding-dialog-seen'] = 1;` in for the LocalSettings file for CheckUser.
    
    Bug: T385708

--- localsettings/extensions-CheckUser.php
+++ localsettings/extensions-CheckUser.php
@@ -4,3 +4,7 @@ $wgGroupPermissions['checkuser'] = [];
 $wgGroupPermissions['checkuser']['checkuser'] = true;
 $wgGroupPermissions['checkuser']['checkuser-log'] = true;
 $wgGroupPermissions['checkuser']['checkuser-temporary-account'] = true;
+
+// Hide the Temporary Accounts onboarding dialog by default for new users,
+// to avoid annoyance when testing features other than this one.
+$wgDefaultUserOptions['checkuser-temporary-accounts-onboarding-dialog-seen'] = 1;
