e99f6ceb9734d0c02c015e3f4b9760c3cb01715c
Ebomani <ebomani@wikimedia.org>
2025-03-20 17:31:49 +0000

    Adding ReadingLists extension to Patch demo and shared-repo
    Bug:T386417

---
+++ localsettings/extensions-ReadingLists.php
@@ -0,0 +1,8 @@
+<?php
+
+$wgReadingListsWeb = true;
+$wgReadingListAndroidAppDownloadLink = "https://play.google.com/store/apps/details?id=org.wikipedia&referrer=utm_source%3DreadingListsShare";
+$wgReadingListiOSAppDownloadLink = "https://itunes.apple.com/app/apple-store/id324715238?pt=208305&ct=readingListsShare";
+
+// Optionally you can also anonymize the import URL (enable anonymized previews)
+$wgReadingListsAnonymizedPreviews = true;
