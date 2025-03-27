061f1a102152fb016c53046a8690db7e2786b78a
Bunnypranav <bunnypranav.wiki@bunnyorg.in>
2025-03-27 15:25:45 +0000

    Additional config for PageTriage extention
    Add more detailed config for PageTriage which better mirrors it to enwiki for easier testing.
    
    Configuration was borrowed from [en:User:Novem Linguae/Essays/Docker tutorial for Windows (WSL)](https://en.wikipedia.org/wiki/User:Novem_Linguae/Essays/Docker_tutorial_for_Windows\_(WSL)#PageTriage).
    
    /cc @jnuche for the review.
    
    Thanks!

--- localsettings/extensions-PageTriage.php
+++ localsettings/extensions-PageTriage.php
@@ -1,5 +1,12 @@
 <?php
 
+// Special:NewPagesFeed has some code that puts "created by new editor" if they are not autoconfirmed. But autoconfirmed needs to be turned on.
+$wgAutoConfirmCount = 10;
+$wgAutoConfirmAge = 4;
+$wgPageTriageEnableCopyvio = true;
+
+// Remove autopatrolled right from sysop, create autoreviewer
+// and patroller groups for better mirroring enwiki.
 $wgGroupPermissions['sysop']['autopatrol'] = false;
 $wgGroupPermissions['autoreviewer']['autopatrol'] = true;
 $wgGroupPermissions['patroller']['patrol'] = true;
